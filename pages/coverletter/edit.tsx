7384ca8b0dc53d605466f06e541aad244c08b2ca
J1min <aoiharu41@gmail.com>
2023-07-06 03:31:23 +0900

    미리 만들어놓기

--- pages/coverletter/edit.tsx
+++ pages/coverletter/edit.tsx
@@ -1,9 +1,26 @@
+import httpClient from "@/apis";
 import Button from "@/components/atoms/Button";
 import Frame from "@/components/atoms/Frame";
 import TextArea from "@/components/atoms/TextArea";
 import ThinkingIcon from "@/components/icons/ThinkingIcon";
+import { coverLetterState } from "@/store/coverletter.store";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
 
 export default function CoverLetterEdit() {
+  const [content, setContent] = useState("");
+  const [coverLetter, setCoverLetter] = useRecoilState(coverLetterState);
+
+  const router = useRouter();
+
+  const handleClick = () => {
+    httpClient.jaso.get().then((r) => {
+      setCoverLetter(r.data);
+      router.push("/coverletter/result");
+    });
+  };
+
   return (
     <Frame>
       <div className="flex items-center gap-4 mt-9">
@@ -11,11 +28,15 @@ export default function CoverLetterEdit() {
         <p className="text-4xl font-black">자기소개서 첨삭 </p>
       </div>
       <div className="mt-10">
-        <TextArea placeholder="자기소개서를 입력해주세요." className="h-96" />
+        <TextArea
+          placeholder="자기소개서를 입력해주세요."
+          className="h-96"
+          onChange={(e) => setContent(e.target.value)}
+        />
       </div>
 
       <div className="flex justify-center mt-14">
-        <Button>첨삭하기</Button>
+        <Button onClick={handleClick}>첨삭하기</Button>
       </div>
     </Frame>
   );
