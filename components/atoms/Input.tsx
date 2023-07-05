cefa2ce34c391df691b040e6f59791e53b95383a
J1min <aoiharu41@gmail.com>
2023-07-05 13:52:27 +0900

    인풋 추가

---
+++ components/atoms/Input.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  tag?: "input" | "textarea";
+  label: string;
+}
+
+export default function Input({
+  id,
+  tag = "input",
+  label,
+  className,
+  ...props
+}: InputProps) {
+  return (
+    <div>
+      <label htmlFor={id} className="">{label}</label>
+      <input
+        id={id}
+        className={classNames("w-full p-[13.5px] mt-2", {}, className)}
+        {...props}
+      />
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import Button from "@/components/atoms/button";
+import Button from "@/components/atoms/Button";
+import Input from "@/components/atoms/Input";
 
 export default function Home() {
   return (
     <div>
-      <Button>완료하기</Button>
+      <Button disabled>완료하기</Button>
+      <Input
+        id="company"
+        placeholder="지원 회사를 기입해주세요"
+        label="지원회사"
+      />
     </div>
   );
 }
