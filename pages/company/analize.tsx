bc0daebca06326e94763eed4a3bcf2d7a52dbc82
J1min <aoiharu41@gmail.com>
2023-07-05 16:03:14 +0900

    내 기업 분석 페이지 추가

---
+++ pages/company/analize.tsx
@@ -0,0 +1,30 @@
+import Frame from "@/components/atoms/Frame";
+import ThinkingIcon from "@/components/icons/ThinkingIcon";
+
+export default function Analize() {
+  return (
+    <Frame>
+      <div className="pt-9">
+        <div className="flex gap-4 items-center">
+          <ThinkingIcon />
+          <span className="text-4xl font-black">내 기업 분석</span>
+        </div>
+
+        <div>
+          <div>
+            <p className="flex flex-col gap-2">한줄소개</p>
+            <p className="p-5 bg-white">
+              어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p>전체소개</p>
+            <p className="p-5 bg-white">
+              어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고어쩌고
+            </p>
+          </div>
+        </div>
+      </div>
+    </Frame>
+  );
+}
