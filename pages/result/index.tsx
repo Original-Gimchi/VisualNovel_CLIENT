d2db62433050c13ea93e1faa925c1022d1ae78b0
J1min <aoiharu41@gmail.com>
2023-07-05 17:47:23 +0900

    완료하기 버튼 위치 수정

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -5,13 +5,26 @@ import TextArea from "@/components/atoms/TextArea";
 import ThinkingIcon from "@/components/icons/ThinkingIcon";
 
 export default function Result() {
+  const categorys = [
+    "#열정",
+    "#성장",
+    "#커뮤니케이션",
+    "#성장",
+    "#커뮤니케이션",
+    "#성장",
+    "#커뮤니케이션",
+    "#성장",
+    "#커뮤니케이션",
+    "#성장",
+    "#커뮤니케이션",
+  ];
   return (
     <Frame>
       <div className="flex flex-col gap-9 my-9">
         <div>
           <div className="flex items-center gap-4 text-4xl font-black">
             <ThinkingIcon />
-            <h1>이력서 결과</h1>
+            <h1>자기소개서 결과</h1>
           </div>
           <div className="text-[#595959] mt-3">
             자기소개서 작성 시작하기 망설여졌나요? 이젠 똑똑한 인공지능 JASO가
@@ -20,19 +33,13 @@ export default function Result() {
         </div>
         <div>
           <p className="mb-2">회사가 원하는 해시태그</p>
-          <Category.Group
-            categorys={[
-              "테스트",
-              "테스트",
-              "테스트",
-              "테스트",
-              "테스트",
-              "테스트",
-            ]}
-          />
+          <Category.Group categorys={categorys} />
         </div>
-        <TextArea label="나의 이력서" isClipBoard />
-        <TextArea label="구글과 나" isClipBoard />
+      </div>
+      <TextArea label="나의 자기소개서" isClipBoard />
+      <div className="h-5" />
+      <TextArea label="구글과 나" isClipBoard />
+      <div className="flex justify-center my-5">
         <Button>완료하기</Button>
       </div>
     </Frame>
